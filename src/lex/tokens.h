333454736b09607c2e5ac435b53121eb6bea969e
Pietro Carrara <pbcarrara@inf.ufrgs.br>
2023-10-25 20:42:36 -0300

    Update language specification

--- src/lex/tokens.h
+++ src/lex/tokens.h
@@ -1,19 +1,19 @@
 #ifndef TOKENS_H
 #define TOKENS_H
 
-// Names and values provided by the professor, so don't modify these unless
-// you wanna risk failing some automated tests and getting low grades :)
-#define KW_CHAR 256
-#define KW_INT  257
-#define KW_REAL 258
-#define KW_BOOL 259
+// Professor-provided constants. Don't modify unless you wanna fail automated tests.
+
+#define KW_CHAR  256
+#define KW_INT   257
+#define KW_FLOAT 258
+
+#define KW_CODE 260
 
 #define KW_IF     261
-#define KW_THEN   262
 #define KW_ELSE   263
-#define KW_LOOP   264
+#define KW_WHILE  264
 #define KW_INPUT  266
-#define KW_OUTPUT 267
+#define KW_PRINT  267
 #define KW_RETURN 268
 
 #define OPERATOR_LE  270
