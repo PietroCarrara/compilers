18a507dc50536e718e9cb112bd8ddb8473af3747
Pietro Carrara <pbcarrara@inf.ufrgs.br>
2023-11-23 23:59:33 -0300

    Remove symbols table

--- src/main.c
+++ src/main.c
@@ -1,5 +1,3 @@
-#include "symbols.h"
-
 #include <errno.h>
 #include <stdio.h>
 #include <string.h>
@@ -7,17 +5,6 @@
 extern int yyparse(void);
 extern FILE* yyin;
 extern int yylineno;
-
-static void print_symbol(SymbolData* data) {
-  match(data->symbol) {
-    of(IntLiteral, i) printf("int(%d)", *i);
-    of(FloatLiteral, f) printf("float(%f)", *f);
-    of(CharLiteral, c) printf("char(%c)", *c);
-    of(StringLiteral, s) printf("string(%s)", *s);
-    of(IdentifierLiteral, n) printf("identifier(%s)", *n);
-  }
-  printf("\n");
-}
 
 int main(int argc, char** argv) {
   if (argc < 2) {
@@ -36,7 +23,5 @@ int main(int argc, char** argv) {
     return 3;
   }
 
-  printf("Recognized symbols:\n");
-  foreach_symbol(print_symbol);
   return 0;
 }
