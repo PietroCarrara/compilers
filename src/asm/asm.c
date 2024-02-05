bfbc7cbc05d1d8ff637a36550fd64a883c32d542
Pietro Carrara <pbcarrara@inf.ufrgs.br>
2024-02-05 06:57:17 -0300

    Start ASM generation

---
+++ src/asm/asm.c
@@ -0,0 +1,3 @@
+#include "asm.h"
+
+void write_asm(Program program, FILE* out) { }
---
+++ src/asm/asm.h
@@ -0,0 +1,10 @@
+#ifndef ASM_H
+#define ASM_H
+
+#include "intermediary-code.h"
+
+#include <stdio.h>
+
+void write_asm(Program, FILE*);
+
+#endif
